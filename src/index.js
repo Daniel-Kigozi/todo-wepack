f2ed5e7f46e9f2edf416a54f6f28144bd6edf2bf
Daniel-Kigozi <kdanalex69@gmail.com>
2022-12-07 09:40:28 +0300

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,41 +1,39 @@
-
 import './style.css';
+
 const divContainer = document.querySelector('.books-container');
 
-
 const TODO = [
-    {
-    "description": "Collecting shopping items",
-    "complete": false,
-    "index": 1
-    },
-    {
-        "description": "solving hackerrank problems",
-        "complete": false,
-        "index": 2
-        },
-        {
-            "description": "Debugging my project",
-            "complete": false,
-            "index": 3
-            },
-            {
-                "description": "Merging another pull request",
-                "complete": false,
-                "index": 4
-                },
-                {
-                    "description": "Preparing for interviews",
-                    "complete": false,
-                    "index": 5
-                    }
-]
+  {
+    description: 'Collecting shopping items',
+    complete: false,
+    index: 1,
+  },
+  {
+    description: 'solving hackerrank problems',
+    complete: false,
+    index: 2,
+  },
+  {
+    description: 'Debugging my project',
+    complete: false,
+    index: 3,
+  },
+  {
+    description: 'Merging another pull request',
+    complete: false,
+    index: 4,
+  },
+  {
+    description: 'Preparing for interviews',
+    complete: false,
+    index: 5,
+  },
+];
 
-const displayTodo = ({ description, index}) => {
-    console.log('todo');
-    const divElement = document.createElement('div');
-    divElement.className = 'first-item';
-    divElement.innerHTML = `
+const displayTodo = ({ description, index }) => {
+  const divElement = document.createElement('div');
+  divElement.className = 'first-item';
+  divElement.innerHTML = `
           <div class="item-details">
           <input type="checkbox" name="" value="" class="complete"> <h4 class="item-desription">${description}</h4>
           
@@ -43,11 +41,10 @@ const displayTodo = ({ description, index}) => {
           
           </div>
                 `;
-  
-    return divElement;
-  };
 
-  TODO.forEach((item) => {
-    divContainer.append(displayTodo(item));
-  });
+  return divElement;
+};
 
+TODO.forEach((item) => {
+  divContainer.append(displayTodo(item));
+});
